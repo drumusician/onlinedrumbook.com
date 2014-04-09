ed7589cf9493d4f3098aa2548e948158da88bc3c
Tjaco Oostdijk <info@drumusician.com>
2014-04-09 23:20:24 +0200

    add Groove model without migration

---
+++ app/controllers/grooves_controller.rb
@@ -0,0 +1,2 @@
+class GroovesController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :grooves
+
   resources :lesson_contents
 
   # The priority is based upon order of creation: first created -> highest priority.
