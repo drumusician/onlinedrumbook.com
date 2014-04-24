18cc74b8a57cd63e30564ed6c399b0b1293580ae
Tjaco Oostdijk <info@drumusician.com>
2014-04-24 23:50:33 +0200

    remove last migration concerning image column

--- db/migrate/20140424214429_change_image_column_in_lesson_content_to_attachment.rb
+++
@@ -1,9 +0,0 @@
-class ChangeImageColumnInLessonContentToAttachment < ActiveRecord::Migration
-  def up
-    change_column :lesson_contents, :image, :attachment
-  end
-
-  def down
-    change_column :lesson_contents, :image, :string
-  end
-end
