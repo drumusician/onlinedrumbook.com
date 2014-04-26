b578c024e16b6603f10f41e9a50f9b9e15064899
Tjaco Oostdijk <info@drumusician.com>
2014-04-26 21:33:41 +0200

    add user validation no blank email and password spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "passwords" do
+    it "needs a password and confirmation to save" do
+      u = User.new(username: "Steve")
+
+      u.save
+      expect(u).to_not be_valid
+    end
+  end
 end
