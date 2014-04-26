ee1f10bb58f6e2100a06234bf9a9985289dba916
Tjaco Oostdijk <info@drumusician.com>
2014-04-26 22:56:33 +0200

    add basic user validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,11 +2,44 @@ require 'spec_helper'
 
 describe User do
   describe "passwords" do
-    it "needs a password and confirmation to save" do
+    it "needs both a password and confirmation to save" do
       u = User.new(username: "Steve")
-
       u.save
       expect(u).to_not be_valid
+
+      u.password = "password"
+      u.password_confirmation = ""
+      u.save
+      expect(u).to_not be_valid
+
+      u.password_confirmation = "password"
+      u.password = "password"
+      u.save
+      expect(u).to be_valid
+    end
+
+    it "needs password and confirmation to match" do
+      u = User.create(
+        username: "Steve",
+        password: "hunter2",
+        password_confirmation: "hunter")
+
+        expect(u).to_not be_valid
+    end
+  end
+
+  describe "authentication" do
+    let(:user) { User.create(
+      username: "Steve",
+      password: "hunter2",
+      password_confirmation: "hunter2") }
+
+    it "authenticates with a correct password" do
+      expect(user.authenticate("hunter2")).to be
+    end
+
+    it "does not authentice with an incorrect password" do
+      expect(user.authenticate("hunter1")).to_not be
     end
   end
 end
