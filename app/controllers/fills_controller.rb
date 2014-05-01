a80a7df9adb571bbe2f27615c88a4e20a5f86f17
Tjaco Oostdijk <info@drumusician.com>
2014-05-01 22:13:11 +0200

    work on fills controller/model

--- app/controllers/fills_controller.rb
+++ app/controllers/fills_controller.rb
@@ -7,4 +7,22 @@ class FillsController < ApplicationController
   def new
     @fill = Fill.new
   end
+
+  def create
+    @fill = Fill.new( fill_params)
+
+    if @fill.save
+      flash[:notice] = "Fill was succesfully saved!"
+      redirect_to fills_path
+    else
+      flash[:error] = "Something went wrong..."
+      render :new
+    end
+  end
+
+  private
+
+  def fill_params
+    params.require(:fill).permit(:image, :length, :audio, :video)
+  end
 end
--- app/models/lesson_content.rb
+++ app/models/lesson_content.rb
@@ -1,3 +1,5 @@
 class LessonContent < ActiveRecord::Base
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   has_and_belongs_to_many :note_values
 end
