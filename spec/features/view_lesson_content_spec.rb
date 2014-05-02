e8f2493268840853ba964f8e4b41187d1b2bf039
Tjaco Oostdijk <info@drumusician.com>
2014-05-02 11:25:13 +0200

    edit lesson content viewing spec

--- spec/features/view_lesson_content_spec.rb
+++ spec/features/view_lesson_content_spec.rb
@@ -3,11 +3,16 @@ require 'spec_helper'
 feature 'View all lesson content' do
 
   scenario 'View all Grooves' do
-    pending
     visit '/'
     click_link 'Grooves'
 
     expect(page).to have_content("Grooves")
+  end
 
+  scenario 'View all Fills' do
+    visit '/'
+    click_link 'Fills'
+
+    expect(page).to have_content("Fills")
   end
 end
